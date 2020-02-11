e46b0dafcf580b92868d53aa31afb36ffb43f619
SmoKE585 <53314888+SmoKE585@users.noreply.github.com>
2020-02-11 16:53:14 +0300

    v.1.2
    Hello world!

--- modules/webcam_recorder/webcam_recorder.class.php
+++ modules/webcam_recorder/webcam_recorder.class.php
@@ -4,7 +4,7 @@ class webcam_recorder extends module {
 		$this->name="webcam_recorder";
 		$this->title="WEBCam Recorder";
 		$this->module_category="<#LANG_SECTION_APPLICATIONS#>";
-		$this->version = '1.0';
+		$this->version = '1.2';
 		$this->checkInstalled();
 	}
 
@@ -95,6 +95,13 @@ class webcam_recorder extends module {
 			$this->redirect("?");
 		}
 		
+		if($this->mode == 'delete_files' && !empty($this->view_mode)) {
+			//Удаляем файлы
+			$data = SQLSelectOne("SELECT * FROM `webcam_recorder` WHERE ID = '".dbSafe($this->view_mode)."' ORDER BY ID");
+			$this->rmRec($data['PATH'].'/');
+			$this->redirect("?");
+		}
+		
 		if($this->view_mode == 'add_camera') {
 			$arrayDevice = explode('crw-', shell_exec('ls -l /dev/ | grep video'));
 			
@@ -118,6 +125,7 @@ class webcam_recorder extends module {
 			global $linked_property1;
 			global $linked_object2;
 			global $linked_property2;
+			global $reacktOn;
 			
 			$rand = rand(1000, 9999);
 			
@@ -141,9 +149,17 @@ class webcam_recorder extends module {
 			$array['BITRATE'] = $kadr;
 			$array['LINKED_OBJECT1'] = $linked_object1;
 			$array['LINKED_PROPERTY1'] = $linked_property1;
-			$array['LINKED_OBJECT2'] = $linked_object2;
-			$array['LINKED_PROPERTY2'] = $linked_property2;
+			if($linked_object1 != $linked_object2) $array['LINKED_OBJECT2'] = $linked_object2;
+			if($linked_property1 != $linked_property2) $array['LINKED_PROPERTY2'] = $linked_property2;
 			$array['ADDTIME'] = date('d.m.Y H:i:s');
+			$array['REAKTON'] = $reacktOn;
+			
+			if ($array['LINKED_OBJECT1'] && $array['LINKED_PROPERTY1']) {
+				addLinkedProperty($array['LINKED_OBJECT1'], $array['LINKED_PROPERTY1'], $this->name);
+			}
+			if ($array['LINKED_OBJECT2'] && $array['LINKED_PROPERTY2'] && $array['LINKED_OBJECT1'].'.'.$array['LINKED_PROPERTY1'] != $array['LINKED_OBJECT2'].'.'.$array['LINKED_PROPERTY2']) {
+				addLinkedProperty($array['LINKED_OBJECT2'], $array['LINKED_PROPERTY2'], $this->name);
+			}
 			
 			SQLInsert('webcam_recorder', $array);
 			
@@ -156,8 +172,17 @@ class webcam_recorder extends module {
 		}
 		
 		//Выгружаем массив камер
-		$out['PROPERTIES'] = SQLSelect("SELECT * FROM `webcam_recorder` ORDER BY ID");
+		$dataInDB = SQLSelect("SELECT * FROM `webcam_recorder` ORDER BY ID");
+		$out['PROPERTIES'] = $dataInDB;
+		foreach($dataInDB as $key => $value) {
+			$scan = scandir($dataInDB[$key]['PATH']);
+			$out['PROPERTIES'][$key]['TOTAL_VIDEO'] = count($scan)-2;
+			$out['PROPERTIES'][$key]['LAST_VIDEO'] = $scan[2];
 			
+			//Узнаем размер папки
+			$out['PROPERTIES'][$key]['PATH_SIZE'] = round($this->getFilesSize($dataInDB[$key]['PATH'])/1000000, 2);
+		}
+		
 		//Флаг на то, есть ли камеры
 		$out['EMPTY_CAMS'] = $this->config['EMPTY_CAMS'];
 		$out['VERSION_MODULE'] = $this->version;
@@ -174,12 +199,40 @@ class webcam_recorder extends module {
 		return false;
 	}
 	
+	function propertySetHandle($object, $property, $value) {
+		$this->getConfig();
+		$properties = SQLSelect("SELECT * FROM `webcam_recorder` WHERE (LINKED_OBJECT1='" . DBSafe($object) . "' AND LINKED_PROPERTY1='" . DBSafe($property) . "') OR (LINKED_OBJECT2='" . DBSafe($object) . "' AND LINKED_PROPERTY2='" . DBSafe($property) . "')");
+		$total = count($properties);
+		
+		if ($total) {
+			for ($i = 0; $i < $total; $i++) {
+				if($value == $properties[$i]['REAKTON'] || $properties[$i]['REAKTON'] == 2) $this->recording($properties[$i]['ID']);
+			}
+		}
+	}
+	
 	function createFolder($path) {
 		//Проверим есть ли папка с сегоднешней датой
 		if(!is_dir($path)) {
 			mkdir($path, 0755, true);
 		} 
 		return !is_dir($path.'/');
+	}
+	
+	function getFilesSize($path) {
+		$fileSize = 0;
+		$dir = scandir($path);
+
+		foreach($dir as $file)
+		{
+			if (($file!='.') && ($file!='..'))
+				if(is_dir($path . '/' . $file))
+					$fileSize += $this->getFilesSize($path.'/'.$file);
+				else
+					$fileSize += filesize($path . '/' . $file);
+		}
+		
+		return $fileSize;
 	}
 	
 	function recording($camID) {
@@ -232,7 +285,27 @@ class webcam_recorder extends module {
 		parent::install();
 	}
 	
+	function DeleteLinkedProperties() {
+		$properties = SQLSelect("SELECT * FROM `webcam_recorder` WHERE (LINKED_OBJECT1 != '' AND LINKED_PROPERTY1 != '') OR (LINKED_OBJECT2 != '' AND LINKED_PROPERTY2 != '')");
+
+		if (!empty($properties)) {
+			foreach ($properties as $prop) {
+				if($prop['LINKED_OBJECT1'] && $prop['LINKED_PROPERTY1']) removeLinkedProperty($prop['LINKED_OBJECT1'], $prop['LINKED_PROPERTY1'], $this->name);
+				if($prop['LINKED_OBJECT2'] && $prop['LINKED_PROPERTY2']) removeLinkedProperty($prop['LINKED_OBJECT2'], $prop['LINKED_PROPERTY2'], $this->name);
+			}
+		}
+	}
+	
 	function uninstall() {
+		//Удалим записи
+		$dataInDB = SQLSelect("SELECT * FROM `webcam_recorder` ORDER BY ID");
+		
+		foreach($dataInDB as $key => $value) {
+			$this->rmRec($dataInDB[$key]['PATH']);
+		}
+		
+		$this->DeleteLinkedProperties();
+		
 		SQLExec('DROP TABLE IF EXISTS webcam_recorder');
 	}	
 	
@@ -252,6 +325,7 @@ webcam_recorder: LINKED_OBJECT1 varchar(255) NOT NULL DEFAULT ''
 webcam_recorder: LINKED_PROPERTY1 varchar(255) NOT NULL DEFAULT ''
 webcam_recorder: LINKED_OBJECT2 varchar(255) NOT NULL DEFAULT ''
 webcam_recorder: LINKED_PROPERTY2 varchar(255) NOT NULL DEFAULT ''
+webcam_recorder: REAKTON varchar(255) NOT NULL DEFAULT ''
 webcam_recorder: ADDTIME varchar(255) NOT NULL DEFAULT ''
 	
 EOD;
